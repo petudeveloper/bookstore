1b224733b4a71ba339025929702afad63d40ec17
petumazo <davidalvarezmazzo@gmail.com>
2021-10-05 21:35:58 -0500

    Update addBook functionality with select genres.

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,9 +1,30 @@
+const bookGenres = [
+  'Action and Adventure',
+  'Classics',
+  'Comic Book or Graphic Novel',
+  'Detective and Mystery',
+  'Fantasy',
+  'Historical Fiction',
+  'Science Fiction',
+  'Horror',
+  'Literary Fiction',
+  'Romance',
+  'Science Fiction (Sci-Fi)',
+  'Suspense and Thrillers',
+];
+
 const AddBook = () => (
   <div>
     <h3>Add New Book</h3>
     <form>
       <input type="text" placeholder="Book Title" />
-      <input type="text" placeholder="Category" />
+      <select className="addBookGenre fontStyle9 fontColor4">
+        {
+          bookGenres.map((genres) => (
+            <option key="book-id" value={genres}>{genres}</option>
+          ))
+        }
+      </select>
       <button type="submit">Add Book</button>
     </form>
   </div>
