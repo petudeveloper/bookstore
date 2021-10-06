a33d2f5c801879a4ad08c2b88b2c5aeb94c1b909
petumazo <davidalvarezmazzo@gmail.com>
2021-10-06 16:29:54 -0500

    Set the data inputs in React state, dispatch the corresponding action.

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -1,3 +1,9 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/books';
+
 const bookGenres = [
   'Action and Adventure',
   'Classics',
@@ -13,21 +19,46 @@ const bookGenres = [
   'Suspense and Thrillers',
 ];
 
-const AddBookForm = () => (
-  <div>
-    <h3>Add New Book</h3>
-    <form className="form">
-      <input type="text" placeholder="Book Title" />
-      <select className="addBookGenre fontStyle9 fontColor4">
-        {
+const AddBookForm = () => {
+  const dispatch = useDispatch();
+  const createBookAction = bindActionCreators(addBook, dispatch);
+
+  const [title, updateTitle] = useState('');
+  const [author, updateAuthor] = useState('');
+
+  const setTitle = (e) => updateTitle(e.target.value);
+  const setAuthor = (e) => updateAuthor(e.target.value);
+
+  const submitBookToStore = (e) => {
+    e.preventDefault();
+    const newBook = {
+      id: uuidv4(), // make sure it's unique
+      title,
+      author,
+    };
+
+    // dispatch an action and pass it the newBook object (your action's payload)
+    createBookAction(newBook);
+    updateTitle('');
+    updateAuthor('');
+  };
+
+  return (
+    <div>
+      <h3>Add New Book</h3>
+      <form className="form" onSubmit={submitBookToStore}>
+        <input type="text" placeholder="Book Title" onChange={setTitle} />
+        <select className="addBookGenre fontStyle9 fontColor4" onChange={setAuthor}>
+          {
           bookGenres.map((genres) => (
             <option key="book-id" value={genres}>{genres}</option>
           ))
         }
-      </select>
-      <button type="submit">Add Book</button>
-    </form>
-  </div>
-);
+        </select>
+        <button type="submit">Add Book</button>
+      </form>
+    </div>
+  );
+};
 
 export default AddBookForm;
