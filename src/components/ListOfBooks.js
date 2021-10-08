48cef77dead521af9b40ee946934c852a2d75cf2
petumazo <davidalvarezmazzo@gmail.com>
2021-10-08 12:09:55 -0500

    Update listOfBooks to use loadBooks action.

--- src/components/ListOfBooks.js
+++ src/components/ListOfBooks.js
@@ -1,18 +1,31 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { useEffect } from 'react';
 import Book from './Book';
 import AddBookForm from './AddBookForm';
+import { loadBooks } from '../redux/books/books';
 
 const ListOfBooks = () => {
   const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  const loadBooksAction = bindActionCreators(loadBooks, dispatch);
+
+  useEffect(() => {
+    loadBooksAction();
+  }, [dispatch]);
 
   return (
     <div className="container-list-books">
       {
-        books.map((book) => (
-          <Book key={book.id} title={book.title} id={book.id} />
+        Object.keys(books).map((id) => (
+          <Book
+            key={id}
+            genre={books[id][0].category}
+            id={id}
+            title={books[id][0].title}
+          />
         ))
       }
-
       <AddBookForm />
     </div>
   );
