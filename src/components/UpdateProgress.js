42d7b31f0e911c696176605ad2b1335eab4470c9
petumazo <davidalvarezmazzo@gmail.com>
2021-10-08 16:30:28 -0500

    Update the style of UpdateProgres element.

--- src/components/UpdateProgress.js
+++ src/components/UpdateProgress.js
@@ -1,20 +1,28 @@
-import '../styles/circle.css';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import { useState } from 'react';
 
-const UpdateProgres = () => (
-  <div className="update-progres">
-    <div className="c100 p12 small">
-      <span>12%</span>
-      <div className="slice">
-        <div className="bar" />
-        <div className="fill" />
+const UpdateProgress = () => {
+  const [progress] = useState(64);
+  return (
+    <div className="update-progres">
+      <div className="progress-bar-container">
+        <CircularProgressbar value={progress} className="circular-progress-bar" />
+        <div className="progress-completed">
+          <span className="progress-in-numbers">
+            {progress}
+            %
+          </span>
+          <span className="completed-label">completed</span>
+        </div>
+      </div>
+      <div className="container-update-btn">
+        <span className="chapter-title">Current Chapter</span>
+        <span className="chapter">Chapter 17</span>
+        <button className="update-progres-btn" type="button">UPDATE PROGRESS</button>
       </div>
     </div>
-    <div className="container-update-btn">
-      <span className="chapter-title">Current Chapter</span>
-      <span className="chapter">Chapter 17</span>
-      <button className="update-progres-btn" type="button">UPDATE PROGRESS</button>
-    </div>
-  </div>
-);
+  );
+};
 
-export default UpdateProgres;
+export default UpdateProgress;
