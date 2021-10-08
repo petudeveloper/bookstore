3d807ac494166d282884067bff5d6fab5e8bf601
petumazo <davidalvarezmazzo@gmail.com>
2021-10-08 12:10:26 -0500

    Update Book to use API info.

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,7 +5,7 @@ import UpdateProgres from './UpdateProgress';
 import { removeBook } from '../redux/books/books';
 
 const Book = (props) => {
-  const { title, id } = props;
+  const { title, id, genre } = props;
 
   const dispatch = useDispatch();
   const removeBookAction = bindActionCreators(removeBook, dispatch);
@@ -13,9 +13,9 @@ const Book = (props) => {
   return (
     <div className="bookInfo">
       <div>
-        <span className="genre-tittle">Fantasy</span>
+        <span className="genre-tittle">{genre}</span>
         <h3>{ title }</h3>
-        <span className="author">Pepito Perez</span>
+        <span className="author">petudeveloper</span>
         <div>
           <button className="comment-btn book-btn" type="button">Comments</button>
           <button className="delete-btn book-btn" type="button" id={`book${id}`} onClick={() => removeBookAction(id)}>Remove</button>
@@ -30,6 +30,7 @@ const Book = (props) => {
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
 };
 
 export default Book;
